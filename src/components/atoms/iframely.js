99145706886afbfbc985812f0a2c45a85492bff6
nakanakamu0828 <yuuki.nakamura.0828@gmail.com>
2019-04-21 13:04:11 +0800

    refactor: エラー修正wq

--- src/components/atoms/iframely.js
+++ src/components/atoms/iframely.js
@@ -1,20 +1,35 @@
 import React from 'react'
 
 class Iframely extends React.Component  {
+  _isMounted = false;
 
-
-  componentDidMount() {
-    window.iframely && window.iframely.load();
+  constructor(...args) {
+    super(...args)
+    this.ref = React.createRef()
   }
 
+  componentDidMount() {
+    this._isMounted = true;
+    const s = document.createElement('script')
+    s.src = '//cdn.iframe.ly/embed.js'
+    s.setAttribute('data-timestamp', + new Date())
+    this.ref.current.appendChild(s)
+    s.onload = () => {
+      window.iframely && window.iframely.load();
+    }
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
 
   getIframelyHtml(href) {
-    return { __html: '<div class="iframely-embed"><div class="iframely-responsive" style="height: 140px; padding-bottom: 0;"><a href="' + href + '" data-iframely-url="//cdn.iframe.ly/lYQs7A9"></a></div></div><script async src="//cdn.iframe.ly/embed.js" charset="utf-8"></script>' }
+    return { __html: '<div class="iframely-embed"><div class="iframely-responsive" style="height: 140px; padding-bottom: 0;"><a href="' + href + '" data-iframely-url="//cdn.iframe.ly/lYQs7A9"></a></div></div>' }
   }
 
   render() {
     const { href } = this.props;
-    return <div dangerouslySetInnerHTML={this.getIframelyHtml(href)} />
+    return <div ref={this.ref} dangerouslySetInnerHTML={this.getIframelyHtml(href)} />
   }
 }
 
