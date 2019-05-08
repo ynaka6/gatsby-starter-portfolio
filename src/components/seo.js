d75644ff437f19a5f38fa0717e7000733529b1ad
nakanakamu0828 <yuuki.nakamura.0828@gmail.com>
2019-05-08 17:32:31 +0800

    refactor: Google search console用の設定追加

--- src/components/seo.js
+++ src/components/seo.js
@@ -67,6 +67,10 @@ function SEO({ description, lang, meta, keywords, title }) {
           name: `twitter:description`,
           content: metaDescription,
         },
+        {
+          name: 'google-site-verification',
+          content: process.env.GOOGLE_SITE_VERIFICATION,
+        },
       ]
         .concat(
           keywords.length > 0
